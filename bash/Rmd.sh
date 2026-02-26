b54d93de16102242522b4339594b285bb93cfee0
chabrault <charlotte.brault@live.com>
2026-02-26 17:52:41 -0600

    add files from T3

---
+++ bash/Rmd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --time=6:00:00 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100g
+#SBATCH --tmp=100g
+#SBATCH --mail-type=END
+#SBATCH --mail-user=cbrault@umn.edu
+#SBATCH --output=GP
+#SBATCH --job-name=GP
+
+
+conda activate r_env
+export RSTUDIO_PANDOC=/users/7/cbrault/miniconda/envs/r_env/bin/
+#export R_LIBS_USER="/users/7/cbrault/R/x86_64-pc-linux-gnu-library/4.4/"
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+
+
+R -e "rmarkdown::render('/users/7/cbrault/Predictathon/analysis/genom_pred.Rmd')"
+
